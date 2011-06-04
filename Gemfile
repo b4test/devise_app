4ca5aa2496ef98429944279794d78d0b547b4c27
unknown <Bora@.(none)>
2011-06-04 17:02:15 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rack', '1.2.3'
+gem 'rake', '0.8.7'
+gem 'rails', '3.0.7'
+
+gem 'sqlite3'
+gem 'will_paginate', '3.0.pre2'
+
+group :development do
+  gem 'rspec-rails', '2.5.0'
+  gem 'annotate-models', '1.0.4'
+  gem 'faker', '0.3.1'
+end
+
+group :test do
+  gem 'rspec', '2.5.0'
+  gem 'webrat', '0.7.1'
+  gem 'factory_girl_rails', '1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseApp::Application.config.secret_token = '55c43f3049cef5780d4c8fbaf72a1b9c86f195b4860d517aa9086d5f89b07fa936486969e4a2c9bebf01548a58b159bbac99254841c5fcca9841a247d24ffee3'
